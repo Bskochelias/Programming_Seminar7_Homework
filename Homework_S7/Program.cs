9314bd96a59ddedb3f8ac4c32f3b6e08ec9f6397
Boleslav <bolik.kg@gmail.com>
2023-02-10 11:41:31 +0600

    Решена первая задача

--- Homework_S7/Program.cs
+++ Homework_S7/Program.cs
@@ -38,6 +38,18 @@ void PrintMas(int [,]arra)
   }
 }
 
+//Замена первой и последней строки
+
+int [,] Chain_first_last_Mass(int [,]arra)
+{
+    for(int j =0;j<arra.GetLength(1);j++)
+      {int temp = arra[0,j];
+      arra[0,j]= arra[arra.GetLength(0)-1,j];
+      arra[arra.GetLength(0)-1,j] = temp;
+      }
+  return arra;
+}
+
 int otvet1 = 0;
 string? otvet2;
 do
@@ -68,7 +80,21 @@ do
   //Начало тела задач
   
   //1. Написать программу, которая обменивает элементы первой строки и последней строки.
+  if (otvet1==1)
+{
+  int m = Proverca_chisla("Введите строки: ");
+  int n = Proverca_chisla("Введите столбцы: ");
+  int min = Proverca_chisla("Введите минмальное значения рандома: ");
+  int max = Proverca_chisla("Введите максимальное значения рандома: ");
+
+  int[,]arra = CreateMass(m,n,min,max);
   
+  System.Console.WriteLine("Получился следующий случайный массив: ");
+  PrintMas(arra);
+  arra = Chain_first_last_Mass(arra);
+  System.Console.WriteLine("После замены получился следующий случайный массив: ");
+  PrintMas(arra);
+}
 
 
   //Конец тела задач    
